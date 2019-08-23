4b8feef0f4ec9aa1048b12b851333c4052b996e9
Josh Hunt <josh@trtr.co>
2019-08-23 11:30:39 +0100

    remove hot loader stuff

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import "lib/autotrack.build";
 import "lib/ls";
-import _ from "lodash";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
 
 import AppRouter from "./AppRouter";
 import "./index.module.scss";
 
-window.lodash = _;
-
 const render = App => {
-  ReactDOM.render(
-    <AppContainer>
-      <App />
-    </AppContainer>,
-    document.getElementById("root")
-  );
+  ReactDOM.render(<App />, document.getElementById("root"));
 };
 
 render(AppRouter);
-
-// Webpack Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept("./AppRouter", () => render(AppRouter));
-}
