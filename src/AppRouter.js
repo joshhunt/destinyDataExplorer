a18edd8fcf6f2e61e35e4c0886acf8ebf406bc83
Josh Hunt <josh@trtr.co>
2019-08-23 11:30:39 +0100

    remove hot loader stuff

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,5 @@
 // eslint-disable no-console
 import React, { Component } from "react";
-import { isString } from "lodash";
 import { Router, Route, browserHistory } from "react-router";
 import { Provider } from "react-redux";
 
@@ -20,27 +19,4 @@ export default class AppRouter extends Component {
       </Provider>
     );
   }
-}
-
-/**
- * Warning from React Router, caused by react-hot-loader.
- * The warning can be safely ignored, so filter it from the console.
- * Otherwise you'll see it every time something changes.
- * See https://github.com/gaearon/react-hot-loader/issues/298
- */
-if (module.hot) {
-  const orgError = console.error;
-  console.error = (...args) => {
-    if (
-      args &&
-      args.length === 1 &&
-      isString(args[0]) &&
-      args[0].indexOf("You cannot change <Router routes>;") > -1
-    ) {
-      // React route changed
-    } else {
-      // Log the error as normally
-      orgError.apply(console, args);
-    }
-  };
 }
