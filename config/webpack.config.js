ff577feb62ad8455e55de6e48c62df31043499aa
Josh Hunt <josh@trtr.co>
2019-08-23 13:27:40 +0100

    Webpack config change: `globalObject: this` for webworkers

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,3 @@
-
-
 const fs = require("fs");
 const isWsl = require("is-wsl");
 const path = require("path");
@@ -191,7 +189,10 @@ module.exports = function(webpackEnv) {
           (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, "/")),
       // Prevents conflicts when multiple Webpack runtimes (from different apps)
       // are used on the same page.
-      jsonpFunction: `webpackJsonp${appPackageJson.name}`
+      jsonpFunction: `webpackJsonp${appPackageJson.name}`,
+
+      // Required for web worker. Perhaps only during development?
+      globalObject: "this"
     },
     optimization: {
       minimize: isEnvProduction,
