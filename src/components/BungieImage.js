97caafbaed29b7c389fb1ed34f9f5ad400cdacca
sundevour <31990469+sundevour@users.noreply.github.com>
2019-08-25 16:03:10 -0700

    don't return bad images

--- src/components/BungieImage.js
+++ src/components/BungieImage.js
@@ -3,5 +3,5 @@ import React from "react";
 import { bungieUrl } from "lib/destinyUtils";
 
 export default function BungieImage({ src, ...props }) {
-  return <img src={bungieUrl(src)} {...props} alt="" />;
+  return (src && (<img src={bungieUrl(src)} {...props} alt="" />)) || null;
 }
