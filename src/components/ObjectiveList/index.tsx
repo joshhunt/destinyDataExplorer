e4f3e1f0d195795b874547aad6049643940d1d01
joshhunt <josh@trtr.co>
2021-02-19 13:15:04 +0000

    polish up objectives

--- src/components/ObjectiveList/index.tsx
+++ src/components/ObjectiveList/index.tsx
@@ -1,10 +1,39 @@
+import { DestinyObjectiveDefinition } from "bungie-api-ts/destiny2";
 import { notEmpty } from "lib/utils";
 import React from "react";
 import { useSelector } from "react-redux";
 import { ReduxStore } from "types";
 
+import s from "./styles.module.scss";
+
 interface ObjectiveListProps {
   objectiveHashes: number[];
+}
+
+enum DestinyUnlockValueUIStyle {
+  Automatic = 0,
+  Fraction = 1,
+  Checkbox = 2,
+  Percentage = 3,
+  DateTime = 4,
+  FractionFloat = 5,
+  Integer = 6,
+  TimeDuration = 7,
+  Hidden = 8,
+  Multiplier = 9,
+  GreenPips = 10,
+  RedPips = 11,
+  ExplicitPercentage = 12,
+  RawFloat = 13,
+}
+
+function isBooleanObjective(objectiveDef: DestinyObjectiveDefinition) {
+  return (
+    ((objectiveDef.valueStyle as unknown) as DestinyUnlockValueUIStyle) ===
+      DestinyUnlockValueUIStyle.Checkbox ||
+    (objectiveDef.completionValue === 1 &&
+      (!objectiveDef.allowOvercompletion || !objectiveDef.showValueOnComplete))
+  );
 }
 
 const ObjectiveList: React.FC<ObjectiveListProps> = ({ objectiveHashes }) => {
@@ -18,13 +47,22 @@ const ObjectiveList: React.FC<ObjectiveListProps> = ({ objectiveHashes }) => {
   });
 
   return (
-    <div>
+    <div className={s.root}>
       {objectives.map((objectiveDef, index) => {
         return (
-          <div key={index}>
-            {objectiveDef.progressDescription ||
-              objectiveDef.displayProperties.name || <em>No name</em>}
-            : {objectiveDef.completionValue}
+          <div className={s.objective} key={index}>
+            <div className={s.label}>
+              {objectiveDef.progressDescription ||
+                objectiveDef.displayProperties.name || <em>No name</em>}
+            </div>
+
+            <div className={s.completionValue}>
+              {isBooleanObjective(objectiveDef) ? (
+                <input type="checkbox" disabled />
+              ) : (
+                objectiveDef.completionValue
+              )}
+            </div>
           </div>
         );
       })}
