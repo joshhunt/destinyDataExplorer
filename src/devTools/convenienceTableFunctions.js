ba260f0d2b38a77ef82e62585d7b6baa6ca6a267
nev <gh@f-m.fm>
2022-01-19 13:51:29 -0800

    i screwed up

--- src/devTools/convenienceTableFunctions.js
+++ src/devTools/convenienceTableFunctions.js
@@ -2,224 +2,224 @@ window.getActivity = function (hash) {
   return window.__definitions.DestinyActivityDefinition[hash];
 };
 window.getAllActivity = function () {
-  return window.__definitions.DestinyActivityDefinition;
+  return Object.values(window.__definitions.DestinyActivityDefinition);
 };
 
 window.getActivityGraph = function (hash) {
   return window.__definitions.DestinyActivityGraphDefinition[hash];
 };
 window.getAllActivityGraph = function () {
-  return window.__definitions.DestinyActivityGraphDefinition;
+  return Object.values(window.__definitions.DestinyActivityGraphDefinition);
 };
 
 window.getActivityMode = function (hash) {
   return window.__definitions.DestinyActivityModeDefinition[hash];
 };
 window.getAllActivityMode = function () {
-  return window.__definitions.DestinyActivityModeDefinition;
+  return Object.values(window.__definitions.DestinyActivityModeDefinition);
 };
 
 window.getActivityModifier = function (hash) {
   return window.__definitions.DestinyActivityModifierDefinition[hash];
 };
 window.getAllActivityModifier = function () {
-  return window.__definitions.DestinyActivityModifierDefinition;
+  return Object.values(window.__definitions.DestinyActivityModifierDefinition);
 };
 
 window.getActivityType = function (hash) {
   return window.__definitions.DestinyActivityTypeDefinition[hash];
 };
 window.getAllActivityType = function () {
-  return window.__definitions.DestinyActivityTypeDefinition;
+  return Object.values(window.__definitions.DestinyActivityTypeDefinition);
 };
 
 window.getArtifact = function (hash) {
   return window.__definitions.DestinyArtifactDefinition[hash];
 };
 window.getAllArtifact = function () {
-  return window.__definitions.DestinyArtifactDefinition;
+  return Object.values(window.__definitions.DestinyArtifactDefinition);
 };
 
 window.getBreakerType = function (hash) {
   return window.__definitions.DestinyBreakerTypeDefinition[hash];
 };
 window.getAllBreakerType = function () {
-  return window.__definitions.DestinyBreakerTypeDefinition;
+  return Object.values(window.__definitions.DestinyBreakerTypeDefinition);
 };
 
 window.getChecklist = function (hash) {
   return window.__definitions.DestinyChecklistDefinition[hash];
 };
 window.getAllChecklist = function () {
-  return window.__definitions.DestinyChecklistDefinition;
+  return Object.values(window.__definitions.DestinyChecklistDefinition);
 };
 
 window.getClass = function (hash) {
   return window.__definitions.DestinyClassDefinition[hash];
 };
 window.getAllClass = function () {
-  return window.__definitions.DestinyClassDefinition;
+  return Object.values(window.__definitions.DestinyClassDefinition);
 };
 
 window.getCollectible = function (hash) {
   return window.__definitions.DestinyCollectibleDefinition[hash];
 };
 window.getAllCollectible = function () {
-  return window.__definitions.DestinyCollectibleDefinition;
+  return Object.values(window.__definitions.DestinyCollectibleDefinition);
 };
 
 window.getDamageType = function (hash) {
   return window.__definitions.DestinyDamageTypeDefinition[hash];
 };
 window.getAllDamageType = function () {
-  return window.__definitions.DestinyDamageTypeDefinition;
+  return Object.values(window.__definitions.DestinyDamageTypeDefinition);
 };
 
 window.getDestination = function (hash) {
   return window.__definitions.DestinyDestinationDefinition[hash];
 };
 window.getAllDestination = function () {
-  return window.__definitions.DestinyDestinationDefinition;
+  return Object.values(window.__definitions.DestinyDestinationDefinition);
 };
 
 window.getEnergyType = function (hash) {
   return window.__definitions.DestinyEnergyTypeDefinition[hash];
 };
 window.getAllEnergyType = function () {
-  return window.__definitions.DestinyEnergyTypeDefinition;
+  return Object.values(window.__definitions.DestinyEnergyTypeDefinition);
 };
 
 window.getEquipmentSlot = function (hash) {
   return window.__definitions.DestinyEquipmentSlotDefinition[hash];
 };
 window.getAllEquipmentSlot = function () {
-  return window.__definitions.DestinyEquipmentSlotDefinition;
+  return Object.values(window.__definitions.DestinyEquipmentSlotDefinition);
 };
 
 window.getFaction = function (hash) {
   return window.__definitions.DestinyFactionDefinition[hash];
 };
 window.getAllFaction = function () {
-  return window.__definitions.DestinyFactionDefinition;
+  return Object.values(window.__definitions.DestinyFactionDefinition);
 };
 
 window.getGender = function (hash) {
   return window.__definitions.DestinyGenderDefinition[hash];
 };
 window.getAllGender = function () {
-  return window.__definitions.DestinyGenderDefinition;
+  return Object.values(window.__definitions.DestinyGenderDefinition);
 };
 
 window.getInventoryBucket = function (hash) {
   return window.__definitions.DestinyInventoryBucketDefinition[hash];
 };
 window.getAllInventoryBucket = function () {
-  return window.__definitions.DestinyInventoryBucketDefinition;
+  return Object.values(window.__definitions.DestinyInventoryBucketDefinition);
 };
 
 window.getInventoryItem = function (hash) {
   return window.__definitions.DestinyInventoryItemDefinition[hash];
 };
 window.getAllInventoryItem = function () {
-  return window.__definitions.DestinyInventoryItemDefinition;
+  return Object.values(window.__definitions.DestinyInventoryItemDefinition);
 };
 
 window.getInventoryItemLite = function (hash) {
   return window.__definitions.DestinyInventoryItemLiteDefinition[hash];
 };
 window.getAllInventoryItemLite = function () {
-  return window.__definitions.DestinyInventoryItemLiteDefinition;
+  return Object.values(window.__definitions.DestinyInventoryItemLiteDefinition);
 };
 
 window.getItemCategory = function (hash) {
   return window.__definitions.DestinyItemCategoryDefinition[hash];
 };
 window.getAllItemCategory = function () {
-  return window.__definitions.DestinyItemCategoryDefinition;
+  return Object.values(window.__definitions.DestinyItemCategoryDefinition);
 };
 
 window.getItemTierType = function (hash) {
   return window.__definitions.DestinyItemTierTypeDefinition[hash];
 };
 window.getAllItemTierType = function () {
-  return window.__definitions.DestinyItemTierTypeDefinition;
+  return Object.values(window.__definitions.DestinyItemTierTypeDefinition);
 };
 
 window.getLocation = function (hash) {
   return window.__definitions.DestinyLocationDefinition[hash];
 };
 window.getAllLocation = function () {
-  return window.__definitions.DestinyLocationDefinition;
+  return Object.values(window.__definitions.DestinyLocationDefinition);
 };
 
 window.getLore = function (hash) {
   return window.__definitions.DestinyLoreDefinition[hash];
 };
 window.getAllLore = function () {
-  return window.__definitions.DestinyLoreDefinition;
+  return Object.values(window.__definitions.DestinyLoreDefinition);
 };
 
 window.getMaterialRequirementSet = function (hash) {
   return window.__definitions.DestinyMaterialRequirementSetDefinition[hash];
 };
 window.getAllMaterialRequirementSet = function () {
-  return window.__definitions.DestinyMaterialRequirementSetDefinition;
+  return Object.values(window.__definitions.DestinyMaterialRequirementSetDefinition);
 };
 
 window.getMetric = function (hash) {
   return window.__definitions.DestinyMetricDefinition[hash];
 };
 window.getAllMetric = function () {
-  return window.__definitions.DestinyMetricDefinition;
+  return Object.values(window.__definitions.DestinyMetricDefinition);
 };
 
 window.getMilestone = function (hash) {
   return window.__definitions.DestinyMilestoneDefinition[hash];
 };
 window.getAllMilestone = function () {
-  return window.__definitions.DestinyMilestoneDefinition;
+  return Object.values(window.__definitions.DestinyMilestoneDefinition);
 };
 
 window.getObjective = function (hash) {
   return window.__definitions.DestinyObjectiveDefinition[hash];
 };
 window.getAllObjective = function () {
-  return window.__definitions.DestinyObjectiveDefinition;
+  return Object.values(window.__definitions.DestinyObjectiveDefinition);
 };
 
 window.getPlace = function (hash) {
   return window.__definitions.DestinyPlaceDefinition[hash];
 };
 window.getAllPlace = function () {
-  return window.__definitions.DestinyPlaceDefinition;
+  return Object.values(window.__definitions.DestinyPlaceDefinition);
 };
 
 window.getPlugSet = function (hash) {
   return window.__definitions.DestinyPlugSetDefinition[hash];
 };
 window.getAllPlugSet = function () {
-  return window.__definitions.DestinyPlugSetDefinition;
+  return Object.values(window.__definitions.DestinyPlugSetDefinition);
 };
 
 window.getPowerCap = function (hash) {
   return window.__definitions.DestinyPowerCapDefinition[hash];
 };
 window.getAllPowerCap = function () {
-  return window.__definitions.DestinyPowerCapDefinition;
+  return Object.values(window.__definitions.DestinyPowerCapDefinition);
 };
 
 window.getPresentationNode = function (hash) {
   return window.__definitions.DestinyPresentationNodeDefinition[hash];
 };
 window.getAllPresentationNode = function () {
-  return window.__definitions.DestinyPresentationNodeDefinition;
+  return Object.values(window.__definitions.DestinyPresentationNodeDefinition);
 };
 
 window.getProgression = function (hash) {
   return window.__definitions.DestinyProgressionDefinition[hash];
 };
 window.getAllProgression = function () {
-  return window.__definitions.DestinyProgressionDefinition;
+  return Object.values(window.__definitions.DestinyProgressionDefinition);
 };
 
 window.getProgressionLevelRequirement = function (hash) {
@@ -228,138 +228,138 @@ window.getProgressionLevelRequirement = function (hash) {
   ];
 };
 window.getAllProgressionLevelRequirement = function () {
-  return window.__definitions.DestinyProgressionLevelRequirementDefinition;
+  return Object.values(window.__definitions.DestinyProgressionLevelRequirementDefinition);
 };
 
 window.getProgressionMapping = function (hash) {
   return window.__definitions.DestinyProgressionMappingDefinition[hash];
 };
 window.getAllProgressionMapping = function () {
-  return window.__definitions.DestinyProgressionMappingDefinition;
+  return Object.values(window.__definitions.DestinyProgressionMappingDefinition);
 };
 
 window.getRace = function (hash) {
   return window.__definitions.DestinyRaceDefinition[hash];
 };
 window.getAllRace = function () {
-  return window.__definitions.DestinyRaceDefinition;
+  return Object.values(window.__definitions.DestinyRaceDefinition);
 };
 
 window.getRecord = function (hash) {
   return window.__definitions.DestinyRecordDefinition[hash];
 };
 window.getAllRecord = function () {
-  return window.__definitions.DestinyRecordDefinition;
+  return Object.values(window.__definitions.DestinyRecordDefinition);
 };
 
 window.getReportReasonCategory = function (hash) {
   return window.__definitions.DestinyReportReasonCategoryDefinition[hash];
 };
 window.getAllReportReasonCategory = function () {
-  return window.__definitions.DestinyReportReasonCategoryDefinition;
+  return Object.values(window.__definitions.DestinyReportReasonCategoryDefinition);
 };
 
 window.getRewardSource = function (hash) {
   return window.__definitions.DestinyRewardSourceDefinition[hash];
 };
 window.getAllRewardSource = function () {
-  return window.__definitions.DestinyRewardSourceDefinition;
+  return Object.values(window.__definitions.DestinyRewardSourceDefinition);
 };
 
 window.getSandboxPerk = function (hash) {
   return window.__definitions.DestinySandboxPerkDefinition[hash];
 };
 window.getAllSandboxPerk = function () {
-  return window.__definitions.DestinySandboxPerkDefinition;
+  return Object.values(window.__definitions.DestinySandboxPerkDefinition);
 };
 
 window.getSeason = function (hash) {
   return window.__definitions.DestinySeasonDefinition[hash];
 };
 window.getAllSeason = function () {
-  return window.__definitions.DestinySeasonDefinition;
+  return Object.values(window.__definitions.DestinySeasonDefinition);
 };
 
 window.getSeasonPass = function (hash) {
   return window.__definitions.DestinySeasonPassDefinition[hash];
 };
 window.getAllSeasonPass = function () {
-  return window.__definitions.DestinySeasonPassDefinition;
+  return Object.values(window.__definitions.DestinySeasonPassDefinition);
 };
 
 window.getSocketCategory = function (hash) {
   return window.__definitions.DestinySocketCategoryDefinition[hash];
 };
 window.getAllSocketCategory = function () {
-  return window.__definitions.DestinySocketCategoryDefinition;
+  return Object.values(window.__definitions.DestinySocketCategoryDefinition);
 };
 
 window.getSocketType = function (hash) {
   return window.__definitions.DestinySocketTypeDefinition[hash];
 };
 window.getAllSocketType = function () {
-  return window.__definitions.DestinySocketTypeDefinition;
+  return Object.values(window.__definitions.DestinySocketTypeDefinition);
 };
 
 window.getStat = function (hash) {
   return window.__definitions.DestinyStatDefinition[hash];
 };
 window.getAllStat = function () {
-  return window.__definitions.DestinyStatDefinition;
+  return Object.values(window.__definitions.DestinyStatDefinition);
 };
 
 window.getStatGroup = function (hash) {
   return window.__definitions.DestinyStatGroupDefinition[hash];
 };
 window.getAllStatGroup = function () {
-  return window.__definitions.DestinyStatGroupDefinition;
+  return Object.values(window.__definitions.DestinyStatGroupDefinition);
 };
 
 window.getTalentGrid = function (hash) {
   return window.__definitions.DestinyTalentGridDefinition[hash];
 };
 window.getAllTalentGrid = function () {
-  return window.__definitions.DestinyTalentGridDefinition;
+  return Object.values(window.__definitions.DestinyTalentGridDefinition);
 };
 
 window.getTraitCategory = function (hash) {
   return window.__definitions.DestinyTraitCategoryDefinition[hash];
 };
 window.getAllTraitCategory = function () {
-  return window.__definitions.DestinyTraitCategoryDefinition;
+  return Object.values(window.__definitions.DestinyTraitCategoryDefinition);
 };
 
 window.getTrait = function (hash) {
   return window.__definitions.DestinyTraitDefinition[hash];
 };
 window.getAllTrait = function () {
-  return window.__definitions.DestinyTraitDefinition;
+  return Object.values(window.__definitions.DestinyTraitDefinition);
 };
 
 window.getUnlock = function (hash) {
   return window.__definitions.DestinyUnlockDefinition[hash];
 };
 window.getAllUnlock = function () {
-  return window.__definitions.DestinyUnlockDefinition;
+  return Object.values(window.__definitions.DestinyUnlockDefinition);
 };
 
 window.getUnlockValue = function (hash) {
   return window.__definitions.DestinyUnlockValueDefinition[hash];
 };
 window.getAllUnlockValue = function () {
-  return window.__definitions.DestinyUnlockValueDefinition;
+  return Object.values(window.__definitions.DestinyUnlockValueDefinition);
 };
 
 window.getVendor = function (hash) {
   return window.__definitions.DestinyVendorDefinition[hash];
 };
 window.getAllVendor = function () {
-  return window.__definitions.DestinyVendorDefinition;
+  return Object.values(window.__definitions.DestinyVendorDefinition);
 };
 
 window.getVendorGroup = function (hash) {
   return window.__definitions.DestinyVendorGroupDefinition[hash];
 };
 window.getAllVendorGroup = function () {
-  return window.__definitions.DestinyVendorGroupDefinition;
+  return Object.values(window.__definitions.DestinyVendorGroupDefinition);
 };
